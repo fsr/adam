eacbc078006e1a0448e97e5c5712a2253d30fb30
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-03-04 16:04:42 +0100

    Separation of parsing logic and GUI frontend - which is now recognizable and provides basic features (import multiple plain text files and generate one combined JSON file)

---
+++ coding_gui.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+from gi.repository import Gtk, Gio, Gdk
+
+import create_coding
+
+class GenerateCodingWindow(Gtk.Window):
+	courses = []
+	
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Coding generation")
+		self.set_size_request(800, 600)
+		
+		# HeaderBar
+		
+		self.hb = Gtk.HeaderBar()
+		self.hb.set_show_close_button(True)
+		self.hb.set_title("Coding generation")
+		self.hb.set_subtitle("Import plain text descriptions on the left")
+		self.set_titlebar(self.hb)
+		
+		self.maximize_button = Gtk.ToggleButton()
+		self.maximize_button.set_image(
+			Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="view-fullscreen"), Gtk.IconSize.BUTTON))
+		self.maximize_button.connect("toggled", self.toggle_window_state)
+		self.hb.pack_end(self.maximize_button)
+		
+		self.connect("window-state-event", self.window_state_changed)
+		
+		self.choose_file_button = Gtk.Button()
+		icon = Gio.ThemedIcon(name="document-open")
+		image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+		self.choose_file_button.add(image)
+		self.choose_file_button.connect("clicked", self.choose_file_click)
+		self.hb.pack_start(self.choose_file_button)
+		
+		# Lower Grid
+		
+		self.paned = Gtk.Paned()
+		self.add(self.paned)
+		
+		self.file_list_store = Gtk.ListStore(str)
+		self.file_list = Gtk.TreeView(self.file_list_store)
+		renderer = Gtk.CellRendererText()
+		column = Gtk.TreeViewColumn("Interpreted files", renderer, text=0)
+		self.file_list.append_column(column)
+		self.file_list.set_hexpand(True)
+		self.file_list.set_vexpand(True)
+		self.file_list.set_size_request(200, -1)
+		self.paned.pack1(self.file_list)
+		
+		self.scrolledwindow = Gtk.ScrolledWindow()
+		self.scrolledwindow.set_hexpand(True)
+		self.scrolledwindow.set_vexpand(True)
+		self.json_text = Gtk.TextView()
+		self.json_text.set_vexpand(True)
+		self.scrolledwindow.add(self.json_text)
+		self.paned.pack2(self.scrolledwindow)
+	
+	def toggle_window_state(self, button):
+		if button.get_active():
+			icon = Gio.ThemedIcon(name="view-restore")
+			self.maximize()
+		else:
+			icon = Gio.ThemedIcon(name="view-fullscreen")
+			self.unmaximize()
+		button.set_image(Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON))
+		
+	def window_state_changed(self, window, event):
+		if event.changed_mask == Gdk.WindowState.MAXIMIZED:
+			if event.new_window_state & Gdk.WindowState.MAXIMIZED:
+				self.maximize_button.set_active(True)
+			else:
+				self.maximize_button.set_active(False)
+		return False
+
+	def refresh_json_output(self, filename):
+		self.courses.extend(create_coding.parse_course_data_from_file(filename))
+		self.json_text.get_buffer().set_text(create_coding.prettyprint_json(self.courses))
+		
+	def choose_file_click(self, widget):
+		dialog = Gtk.FileChooserDialog("Choose course file", self,
+			Gtk.FileChooserAction.OPEN,
+			(Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+		response = dialog.run()
+		if response == Gtk.ResponseType.OK:	
+			self.file_list_store.append([dialog.get_filename()[::-1].split("/",1)[0][::-1]])
+			self.refresh_json_output(dialog.get_filename())
+		
+		dialog.destroy()
+
+def show_coding_window():
+	win = GenerateCodingWindow()
+	win.connect("delete-event", Gtk.main_quit)
+	win.show_all()
+	Gtk.main()
+
+show_coding_window()
--- create_coding.py
+++ create_coding.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from gi.repository import Gtk, Gio
 import json
 
 def parse_course_data_from_file(filename):
@@ -65,83 +64,15 @@ def parse_course_data_from_file(filename):
 			code += 1
 			
 	return courses
+	
+def prettyprint_json(courses):
+	return json.dumps(courses, indent=4, sort_keys=True)
+	
+def cli_main():
+	print("Please use the GUI for now.")
+	#courses = parse_course_data_from_file("example_files/course_abc.txt")
+	#courses.extend(parse_course_data_from_file("example_files/course_xyz.txt"))
+	#prettyprint_json(courses)
 
-class GenerateCodingWindow(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Generate coding")
-		
-		# HeaderBar
-		
-		self.hb = Gtk.HeaderBar()
-		self.hb.props.show_close_button = True
-		self.hb.props.title = "HeaderBar example"
-		self.set_titlebar(self.hb)
-		
-		self.choose_file_button = Gtk.Button()
-		icon = Gio.ThemedIcon(name="document-open")
-		image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
-		self.choose_file_button.add(image)
-		self.choose_file_button.connect("clicked", self.choose_file_click)
-		self.hb.pack_start(self.choose_file_button)
-		
-		# Lower Grid
-		
-		self.table = Gtk.Grid()
-		self.table.set_column_homogeneous(True)
-		self.add(self.table)
-		
-		self.file_list = Gtk.TreeView()
-		self.file_list.set_vexpand(True)
-		self.table.attach(self.file_list, 0, 0, 1, 1)
-		
-		self.scrolledwindow = Gtk.ScrolledWindow()
-		self.scrolledwindow.set_hexpand(True)
-		self.scrolledwindow.set_vexpand(True)
-		self.json_text = Gtk.TextView()
-		self.json_text.set_vexpand(True)
-		self.scrolledwindow.add(self.json_text)
-		self.table.attach(self.scrolledwindow, 1, 0, 1, 1)
-		
-		self.file_choice_box = Gtk.Box(spacing=6)
-		self.table.attach(self.file_choice_box, 0, 1, 2, 1)
-		
-		self.filename_entry = Gtk.Entry(text="example_files/course_abc.txt")
-		self.file_choice_box.pack_start(self.filename_entry, True, True, 0)
-		
-		self.load_courses_button = Gtk.Button(label="Load")
-		self.load_courses_button.connect("clicked", self.load_courses)
-		self.file_choice_box.pack_start(self.load_courses_button, True, True, 0)
-		
-		self.generate_coding_button = Gtk.Button(label="Generate coding")
-		self.generate_coding_button.connect("clicked", self.print_coding)
-		self.file_choice_box.pack_start(self.generate_coding_button, True, True, 0)
-		
-
-	def choose_file_click(self, widget):
-		dialog = Gtk.FileChooserDialog("Choose course file", self,
-			Gtk.FileChooserAction.OPEN,
-			(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-			 Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-
-		response = dialog.run()
-		if response == Gtk.ResponseType.OK:
-			self.filename_entry.set_text(dialog.get_filename())
-
-		dialog.destroy()
-		
-	def load_courses(self, widget):
-		parse_course_data_from_file(self.filename_entry.get_text())
-
-	def print_coding(self, widget):
-		pprint.pprint(courses)
-
-def show_coding_window():
-	win = GenerateCodingWindow()
-	win.connect("delete-event", Gtk.main_quit)
-	win.show_all()
-	Gtk.main()
-
-show_coding_window()
-courses = parse_course_data_from_file("example_files/course_abc.txt")
-courses.extend(parse_course_data_from_file("example_files/course_xyz.txt"))
-print(json.dumps(courses, indent=4, sort_keys=True))
+if __name__ == '__main__':
+	cli_main()
