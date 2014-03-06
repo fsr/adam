cfeae08ff33ed4397676f0d9c6e29edbdab8837f
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-03-06 14:21:40 +0100

    JSON export into plain text or fancy html pages usable for printing. Also small fixes elsewhere and README expansion.

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ A basic GUI written for GTK3 is available.
 Usage
 -----
 
-create_coding.py can load multiple plain text files containing data about courses and their tutors and produce the corresponding JSON coding.
+### Create tutor coding ###
+
+#### JSON coding creation ####
+
+`create_coding.py` can load multiple plain text files containing data about courses and their tutors and produce the corresponding JSON coding.
 This JSON data can be printed to stdout:
 
     # Create coding from one or multiple plain text files
@@ -18,6 +22,21 @@ Alternatively you can specify a file to save to save the coding to:
 
     create_coding.py course_abc.txt course_xyz.txt -o file.json
 
-If you want to / can use the GUI, call:
+If you want to / can use the GUI, simply call `coding_gui.py`:
 
     coding_gui.py
+    
+#### Export into readable formats ####
+
+`export_coding.py` takes the created JSON files and produces readable files from it:
+either a simple plain text file
+
+    export_coding.py plain -o outfile coding.json
+
+or a fancy html table (that can be used for printing and showing in the lectures)
+
+    export_coding.py html -o outfile.htm -s example_files/slides.css coding.json
+
+### Import CSV into sqlite database ###
+
+`create_table.py` *TODO: proper CLI*
--- create_coding.py
+++ create_coding.py
@@ -2,56 +2,58 @@
 import json
 import argparse
 
+# Creates a course-data coding (JSON) from plain text input
+
 def parse_questions_from_file(filename):
 	questions = ["foo"]
-	
+	# TODO (obviously)
 	return questions
 
-def parse_course_data_from_file(filename):
+def parse_course_data_from_file(f):
 	# Tail-recursionesque ideas for the win.
 	courses = []
-	with open(filename, "r") as course_file:
-		for line in course_file:
-			# Remove newline at the end
-			line = line[:-1]
-			# Filter comments
-			if line.startswith("#") or not line:
-				continue
-				
-			# New course has started, start new course block
-			if not line.startswith("\t"):
-				course_name = (line[::-1].split("(", 1))[1][::-1].strip()
-				course_lect = (line[::-1].split("(", 1))[0][:0:-1].strip()
-				courses.append({"name": course_name, "lecturer": course_lect, "tutors": []})
-				continue
-				
-			# So we have at least one indentation level, trim it:
-			line = line[1:]
-			# New tutor starts
-			if not line.startswith("\t"):
-				reversed_name_array = line[::-1].split(" ", 1)
-				if len(reversed_name_array) == 1:
-					first = ""
-					last = line
-				else:
-					first = reversed_name_array[1][::-1].strip()
-					last  = reversed_name_array[0][::-1].strip()
-				courses[-1]["tutors"].append({"firstname": first, "lastname": last, "exercises": []})
-				continue
+	for line in f:
+		# Remove newline at the end
+		line = line[:-1]
+		# Filter comments
+		if line.startswith("#") or not line:
+			continue
 			
-			# Two tabs or more means we have to parse exercises
-			exercise = line.strip().split(",",-1)
-			if len(exercise) != 3: raise Exception("Line '" + line + "'is not a valid exercise")
+		# New course has started, start new course block
+		if not line.startswith("\t"):
+			course_name = (line[::-1].split("(", 1))[1][::-1].strip()
+			course_lect = (line[::-1].split("(", 1))[0][:0:-1].strip()
+			courses.append({"name": course_name, "lecturer": course_lect, "tutors": []})
+			continue
 			
-			if   exercise[0] in ["Monday",    "Montag",     "Mo", "Mo", "Mon"]: weekday = 0
-			elif exercise[0] in ["Tuesday",   "Dienstag",   "Tu", "Di", "Tue"]: weekday = 1
-			elif exercise[0] in ["Wednesday", "Mittwoch",   "We", "Mi", "Wed"]: weekday = 2
-			elif exercise[0] in ["Thursday",  "Donnerstag", "Th", "Do", "Thu"]: weekday = 3
-			elif exercise[0] in ["Friday",    "Freitag",    "Fr", "Fr", "Fri"]: weekday = 4
-			else: raise Exception("Weekday '" + exercise[0] + "' is not valid")
-			
-			courses[-1]["tutors"][-1]["exercises"].append(
-				{"day": weekday, "time": int(exercise[1].strip()[:1]), "place": exercise[2].strip()})
+		# So we have at least one indentation level, trim it:
+		line = line[1:]
+		# New tutor starts
+		if not line.startswith("\t"):
+			reversed_name_array = line[::-1].split(" ", 1)
+			if len(reversed_name_array) == 1:
+				first = ""
+				last = line
+			else:
+				first = reversed_name_array[1][::-1].strip()
+				last  = reversed_name_array[0][::-1].strip()
+			courses[-1]["tutors"].append({"firstname": first, "lastname": last, "exercises": []})
+			continue
+		
+		# Two tabs or more means we have to parse exercises
+		exercise = line.strip().split(",",-1)
+		if len(exercise) != 3: raise Exception("Line '" + line + "'is not a valid exercise")
+		
+		if   exercise[0] in ["Monday",    "Montag",     "Mo", "Mo", "Mon"]: weekday = 0
+		elif exercise[0] in ["Tuesday",   "Dienstag",   "Tu", "Di", "Tue"]: weekday = 1
+		elif exercise[0] in ["Wednesday", "Mittwoch",   "We", "Mi", "Wed"]: weekday = 2
+		elif exercise[0] in ["Thursday",  "Donnerstag", "Th", "Do", "Thu"]: weekday = 3
+		elif exercise[0] in ["Friday",    "Freitag",    "Fr", "Fr", "Fri"]: weekday = 4
+		else: raise Exception("Weekday '" + exercise[0] + "' is not valid")
+		
+		courses[-1]["tutors"][-1]["exercises"].append(
+			{"day": weekday, "time": int(exercise[1].strip()[:1]), "place": exercise[2].strip()})
+	f.close()
 	
 	# Gonna use this soon.
 	def exercise_timevalue(e):
@@ -73,8 +75,8 @@ def parse_course_data_from_file(filename):
 
 def course_file_list_to_json(filelist):
 	courses = []
-	for fn in filelist:
-		courses.extend(parse_course_data_from_file(fn))
+	for f in filelist:
+		courses.extend(parse_course_data_from_file(f))
 	return prettyprint_json(courses)
 
 def prettyprint_json(data):
@@ -86,18 +88,18 @@ if __name__ == '__main__':
 		description="Create JSON coding for courses (or optionally questions) from plain text files.")
 	parser.add_argument("-t", "--type", choices=["courses", "questions"],
 		default="courses", help="data that is to be parsed (default: courses)")
-	parser.add_argument('inputfiles', nargs='+', metavar="inputfile", help="plain text data file(s)")
-	parser.add_argument("-o", "--output", nargs=1, help="JSON file the output is to be stored in")
+	parser.add_argument('inputfiles', nargs='+', type=argparse.FileType('r'), metavar="inputfile", help="plain text data file(s)")
+	parser.add_argument("-o", "--output", type=argparse.FileType('w'), nargs=1, help="JSON file the output is to be stored in")
 	
 	args = parser.parse_args()
 	
 	if args.type == "questions":
-		json_coding = prettyprint_json(parse_questions_from_file((args.inputfiles)))
+		json_coding = prettyprint_json(parse_questions_from_file(args.inputfiles))
 	else:
 		json_coding = course_file_list_to_json(args.inputfiles)
 	
 	if not args.output:
 		print(json_coding)
 	else:
-		with open(args.output[0], 'w') as f:
-			f.write(json_coding)
+		args.output[0].write(json_coding)
+		args.output[0].close()
