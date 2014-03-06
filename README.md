57b1cbbcc2886d02087df15d682af314db91fa3d
Sebastian J. Mielke <sjm@sjmmusic.de>
2014-03-06 09:47:27 +0100

    README fixes and fancier shorter code for the table generation

--- README.md
+++ README.md
@@ -3,18 +3,18 @@ Adam
 
 Adam is a tool usable for the teaching evaluation at the TU Dresden.
 This version is completely rewritten using Python3 based on the most simple and basic requirements.
-A basic GUI written in GTK3 is available.
+A basic GUI written for GTK3 is available.
 
 Usage
 -----
 
-create_coding.py can load multiple plain text files and produce the corresponding JSON coding.
-This JSON data can be printed to stdout and redirected into a file by the user using the shell:
+create_coding.py can load multiple plain text files containing data about courses and their tutors and produce the corresponding JSON coding.
+This JSON data can be printed to stdout:
 
-    # Create coding from one or multiple plain text files and save to file.json
+    # Create coding from one or multiple plain text files
     create_coding.py course_abc.txt course_xyz.txt > file.json
 
-Alternatively you can specify a file to save to save the configuration to:
+Alternatively you can specify a file to save to save the coding to:
 
     create_coding.py course_abc.txt course_xyz.txt -o file.json
 
--- create_table.py
+++ create_table.py
@@ -4,36 +4,27 @@ import sqlite3 as lite
 import sys
 
 con = lite.connect('TestDB.db')
-f = open("test_data.csv","r")
+f = open("real_files/test_data.csv","r")
 lectureName = "Vorlesung1"
 
 header = f.readline().split(",")
-header[0] = header[0].replace(" ", "")
-header[-1] = header[-1].rstrip('\n')
+header = list(map(lambda s: ''.join(s.split()), header))
 
 with con:
 
 	cur = con.cursor()
  	
- 	#Name the table columns
+ 	# Name the table columns
 	cur.execute("DROP TABLE IF EXISTS %s" % lectureName)
-	cur.execute("CREATE TABLE %s(id INT)" % lectureName)
-	for titel in header:
-		cur.execute("ALTER TABLE %s ADD COLUMN %s INT" % (lectureName,titel))
+	types = ""
+	for title in header:
+		types += "%s INT, " % title
+	cur.execute("CREATE TABLE %s(%s)" % (lectureName, types[:-2]))
 
 
-	#insert values into the columns
-	i = 0
-	header.insert(0, "id")
-
+	# insert values into the columns
+	# Note: sqlite automatically adds a (semi-hidden) ROWID column for all rows!
 	for lines in f:
-		values = lines.split(",")
-		values.insert(0, i)
-		i = i + 1
-		values[-1] = values[-1].rstrip('\n')
-
-		cur.execute("INSERT INTO %s %s VALUES %s" % (lectureName,tuple(header),tuple(values)))
-		
-
-
-
+		values = list(map(lambda s: s.strip(), lines.split(",")))
+		cur.execute("INSERT INTO %s %s VALUES %s" %
+			(lectureName, tuple(header), tuple(values)))
