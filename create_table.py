f082ff070c0a8c98359eecc1475ec31febe6fe3d
Hannibal42 <simon.hanisch@gmx.de>
2014-03-07 13:22:51 +0100

    fixed the table name problem.

--- create_table.py
+++ create_table.py
@@ -1,29 +1,47 @@
 #!usr/bin/python3
 
 import sqlite3 as lite
+import argparse
+import re
 
-con = lite.connect('TestDB.db')
-f = open("real_files/test_data.csv","r")
-lectureName = "Vorlesung1"
+def create_table(lectureName, inputfile, db):
 
-header = f.readline().split(",")
-header = list(map(lambda s: ''.join(s.split()), header))
+	con = lite.connect("{}".format(db))
 
-with con:
+	with open(inputfile,"r") as f:
+		header = f.readline().split(",")
+		header = list(map(lambda s: ''.join(s.split()), header))
 
-	cur = con.cursor()
- 	
- 	# Name the table columns
-	cur.execute("DROP TABLE IF EXISTS %s" % lectureName)
-	types = ""
-	for title in header:
-		types += "%s INT, " % title
-	cur.execute("CREATE TABLE %s(%s)" % (lectureName, types[:-2]))
+		with con:
+
+			cur = con.cursor()
+			
+			# Name the table columns
+			cur.execute("DROP TABLE IF EXISTS {}".format(lectureName))
+			types = ""
+			for title in header:
+				types += "{} INT, ".format(title)
+			cur.execute("CREATE TABLE {}({})".format(lectureName, types[:-2]))
 
 
-	# insert values into the columns
-	# Note: sqlite automatically adds a (semi-hidden) ROWID column for all rows!
-	for lines in f:
-		values = list(map(lambda s: s.strip(), lines.split(",")))
-		cur.execute("INSERT INTO %s %s VALUES %s" %
-			(lectureName, tuple(header), tuple(values)))
+			# insert values into the columns
+			# Note: sqlite automatically adds a (semi-hidden) ROWID column for all rows!
+			for lines in f:
+				values = list(map(lambda s: s.strip(), lines.split(",")))
+				cur.execute("INSERT INTO {} {} VALUES {}".format(lectureName, tuple(header), tuple(values)))
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description="Import .csv import into a sqllite3 database.")
+	
+	parser.add_argument("-d","--database", nargs=1, help="The sqllite3 db for the output", required=True)
+	parser.add_argument("-i","--importfile", nargs="+" ,help="1-Many imput .csv imports", required=True)
+
+	args = parser.parse_args()
+	db = args.database[0]
+
+	for arg in args.importfile:
+		if arg.endswith(".CSV"):
+			name = re.sub('[\W_]+','',arg) # removes all the none alphanumeric chars from the string
+			create_table(name,arg,db)
