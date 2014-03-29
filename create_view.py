26adbdf78bbdd77120502890e680e71368bef1ce
Hannibal42 <hanischsimon@gmail.com>
2014-03-29 21:00:18 +0100

    modified create_view.py to get better results.

--- create_view.py
+++ create_view.py
@@ -25,51 +25,53 @@ def sum_numbers(answers):
 
 
 def create_blank_bardiagram(height,width):
-	svgString = '<g id="diagra">'
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="0" x2="{0}" y1="{1}" y2="{1}" />\n'.format(width,height*0.9)
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{0}" y1="0" y2="{1}" />\n'.format(width*0.1,height)
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.08,width*0.12,height*0.7)
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.08,width*0.12,height*0.5)
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.08,width*0.12,height*0.3)
-	svgString += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.08,width*0.12,height*0.1)
-	svgString += '</g>'
+	svg = '<g id="diagram">'
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="0" x2="{0}" y1="{1}" y2="{1}" />\n'.format(width,height*0.9)
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{0}" y1="0" y2="{1}" />\n'.format(width*0.1,height)
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.09,width*0.11,height*0.7)
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.09,width*0.11,height*0.5)
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.09,width*0.11,height*0.3)
+	svg += '<line stroke="rgb(10%,10%,16%)" x1="{0}" x2="{1}" y1="{2}" y2="{2}" />\n'.format(width*0.09,width*0.11,height*0.1)
+	svg += '</g>'
 	
-	return svgString
+	return svg
 
-def y_axis_name(maxNumber,height,width):
-	svgString = '<g id="YAxisName">'
-	svgString += '<text x="{}" y="{}" textLength="{}">{}</text> \n'.format(width*0.02,height*0.7,width*0.04,maxNumber*0.25)
-	svgString += '<text x="{}" y="{}" textLength="{}">{}</text> \n'.format(width*0.02,height*0.5,width*0.04,maxNumber*0.5)
-	svgString += '<text x="{}" y="{}" textLength="{}">{}</text> \n'.format(width*0.02,height*0.3,width*0.04,maxNumber*0.75)
-	svgString += '<text x="{}" y="{}" textLength="{}">{}</text> \n'.format(width*0.02,height*0.1,width*0.04,maxNumber)
-	svgString += '</g>'
+def y_axis_name(maxValue,numberOfValues,height,width):
+	cursor = 0
 
-	return svgString
+	svg = '<g id="yAxisLegend">'
+	svg += '<text x="{}" y="{}">{:.0f}</text> \n'.format(width*0.05,height*0.705,maxValue*0.25)
+	svg += '<text x="{}" y="{}">{:.0f}</text> \n'.format(width*0.05,height*0.505,maxValue*0.5)
+	svg += '<text x="{}" y="{}">{:.0f}</text> \n'.format(width*0.05,height*0.305,maxValue*0.75)
+	svg += '<text x="{}" y="{}">{:.0f}</text> \n'.format(width*0.05,height*0.105,maxValue)
+	svg += '</g>'
+
+	return svg
 
 def x_axis_bars(answers,height,width): #TODO: Bars und Beschriftung auseinanderfummeln
 	barProcent = width*(0.7 / len(answers))
 	scale = (1/max_number(answers)) * (height * 0.8)
 	cursor = width*0.12
-	svgString = '<g id="bars">'
-
+	svg = '<g id="bars">'
+	#textLength="{2}"
 	for answer in answers:
 		barheight = answer["number"] * scale
-		svgString += '<text x="{0}" y="{1}" textLength="{2}">{3}</text> \n'.format(cursor,height*0.95,height*0.1,answer["text"])
-		svgString +='<rect x="{0}" y="{1}" width="{2}" height="{3}" style="fill:rgb(0,0,139);" />\n'.format(cursor,(height*0.9)-barheight,barProcent*0.8,barheight)
+		svg += '<text x="{0}" y="{1}" textLength="50" lengthAdjust="spacing" >{3}</text> \n'.format(cursor,height*0.95,height*0.1,answer["text"])
+		svg +='<rect x="{0}" y="{1}" width="{2}" height="{3}" style="fill:rgb(0,0,139);" />\n'.format(cursor,(height*0.9)-barheight,barProcent*0.8,barheight)
 		cursor += barProcent
 
-	svgString += "</g>"
-	return svgString
+	svg += "</g>"
+	return svg
 
 	
 
 def create_bardiagram(question,height,width):
-	svgString ='<svg baseProfile="tiny" height="100%" version="1.2" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink"><defs />\n'
-	svgString += create_blank_bardiagram(height,width)
-	svgString += y_axis_name(max_number(question["answers"]),height,width)
-	svgString += x_axis_bars(question["answers"],height,width)
-	svgString += '</svg>'
-	return svgString
+	svg ='<svg baseProfile="tiny" height="100%" version="1.2" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink"><defs />\n'
+	svg += create_blank_bardiagram(height,width)
+	svg += y_axis_name(max_number(question["answers"]),4,height,width)
+	svg += x_axis_bars(question["answers"],height,width)
+	svg += '</svg>'
+	return svg
 	
 
 
@@ -79,19 +81,18 @@ def create_report_svg(reportJSON,outputfile):
 
 		report = json.load(f)
 		i = 0
-		htmlString = '<html><head><title>A report</title></head><body>'
+		html = '<html><head><title>A report</title></head><body>\n <ul> \n'
 
 		for question in report:
-			i += 1 
 
-			htmlString += '<div id="{}{}">\n'.format(question["view"],str(i))
-			htmlString += '<h1>{}</h1>\n'.format(question["question"])
-			htmlString += create_bardiagram(question,700,700)
-			htmlString += '</div>\n'
+			html += '<li>\n'
+			html += '<h1>{}</h1>\n'.format(question["question"])
+			html += create_bardiagram(question,700,700)
+			html += '</li>\n'
 
-		htmlString += '</body>\n</html>'
+		html += '<ul>\n</body>\n</html>'
 		with open(outputfile,'w') as f:
-			f.write(htmlString)
+			f.write(html)
 
 if __name__ == '__main__':
 
