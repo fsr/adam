59a257d9fe69a52fe8074b312fe1f27a883c231d
Simon Hanisch <hanischsimon@gmail.com>
2014-04-03 18:10:04 +0200

    blank diagram, documented feature from construct_report.py

--- create_view.py
+++ create_view.py
@@ -1,4 +1,4 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 import json
 import argparse
@@ -25,28 +25,32 @@ def sum_numbers(answers):
 
 #parameter: ctx = context Obj, height = abs. height of the dia, width = abs. width of the dia
 def create_blank_bardiagram(ctx,height,width):
+	ctx.save()
+
 	ctx.rel_move_to(0.1*width,0.1*height)   #TODO: Fancy arrowheads for the axis ends
 	ctx.rel_line_to(0,0.8*height)
 	ctx.rel_move_to(-0.1*width,-0.1*height)
 	ctx.rel_line_to(width,0)
-	ctx.stroke()#TODO: Replace with a move_to old position
-
+	
+	ctx.restore()
 
 def y_axis_label(ctx,maxValue,numberOfValues,height,width):
-	distanceMarker = (0.9*(0.7*height)) / numberOfValues
-	ctx.rel_move_to(0.125*width,0.8*height) 
+	ctx.save()
+
+	distanceMarker = (0.9*(0.7*height)) / numberOfValues 
 	differenceValue = maxValue / numberOfValues
 	value = differenceValue
+	ctx.rel_move_to(0.125*width,0.8*height)
 
 	for _ in range(numberOfValues):
+			ctx.save()
+
 			ctx.rel_move_to(-0.05*width,-distanceMarker)
 			ctx.rel_line_to(0.05*width,0)
-			ctx.rel_move_to(-0.125*width,0.25*distanceMarker)
-			ctx.show_text('{}'.format(int(value)))
-			ctx.rel_move_to(,-0.25*distanceMarker)
-			value += differenceValue
 
-	ctx.stroke() #TODO: Replace with a move_to old position
+			ctx.restore()
+
+	ctx.restore()
 
 def x_axis_label(answers,height,width): 
 	#TODO: Implementation
@@ -56,15 +60,28 @@ def create_bars(answers,height,width):
 	#TODO: Implementation
 	return 0
 
-def create_bardiagram(question,height,width):
-	#TODO: Implementation
-	return 0
+def create_bardiagram(ctx,question,height,width):
+	
+	ctx.rectangle(0,0,200,200)
+
+	ctx.move_to(0,0)
+	create_blank_bardiagram(ctx,200,200)
+	ctx.stroke()
+	ctx.move_to(0,0)
+	y_axis_label(ctx,200,4,200,200)
+	ctx.stroke()
+
 	
 
 
 def create_report_pdf(reportJSON,outputfile):
-	#TODO: Implementation
-	return 0
+	mySurface = cairo.PDFSurface(outputfile,595,842)
+	myContext = cairo.Context(mySurface)
+
+	create_bardiagram(myContext,"Test", 100,100)
+	
+	
+
 
 	
 		
@@ -77,15 +94,8 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-	mySurface = cairo.PDFSurface("Test.pdf",595,842)
-	myContext = cairo.Context(mySurface)
 	
-	myContext.rectangle(0,0,200,200)
 
-	myContext.move_to(0,0)
-	create_blank_bardiagram(myContext,200,200)
-	myContext.move_to(0,0)
-	y_axis_label(myContext,200,4,200,200)
 
 
 	create_report_pdf(args.input[0],args.output[0])
